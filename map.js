4437eeb972675b459b8fa01911b5aa0c05194f56
OMIDALAM01 <oalamdar@ucsd.edu>
2025-02-21 21:02:08 -0800

    Update maps.js

--- map.js
+++ map.js
@@ -8,3 +8,27 @@ const map = new mapboxgl.Map({
     minZoom: 5,
     maxZoom: 18
 });
+
+map.on('load', () => {
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
+    });
+
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://opendata.cambridgema.gov/datasets/bike-lanes.geojson'
+    });
+    
+    map.addLayer({
+        id: 'cambridge-bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': '#32D400',
+            'line-width': 5,
+            'line-opacity': 0.6
+        }
+    });
+});
+
